48727e391a1ff4822a34d10e28a90a947808cb9f
tiago <searapaixao@gmail.com>
2018-02-01 16:22:59 +0100

    adding the testnet

---
+++ my_python_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from steembase.account import PasswordKey
+import steembase
+import steem
+steembase.chains.known_chains['STEEM'] = {
+    'chain_id': '79276aea5d4877d9a25892eaa01b0adf019d3e5cb12a97478df3298ccdd01673',
+    'prefix': 'STX', 'steem_symbol': 'STEEM', 'sbd_symbol': 'SBD', 'vests_symbol': 'VESTS'
+}
+
+client = steem.Steem(['https://testnet.steem.vc'])
+
+balance = client.get_account('tiago')['balance']
+
+# print(balance)
+
+# key_dict = client.get_account('tiago').keys()
+
+account = 'tiago'
+password = 'paixao'
+key_types = ['posting','active','owner','memo','foobar']
+
+print(account,'\n',password)
+
+for key_type in key_types:
+    private_key = PasswordKey(account, password, key_type).get_private_key()
+    public_key = private_key.pubkey
+
+    print('Private ' + key_type + ' key: ' + str(private_key))
+    print('Public ' + key_type + ' key: ' + str(public_key) + '\n')
+
+
+
